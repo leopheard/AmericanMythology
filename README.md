3b88d054687cf732ba559c17b44d7f13528ae945
leopheard <leopheard.misc@gmail.com>
2019-08-03 17:58:05 -0400

    upload

---
+++ README.md
@@ -0,0 +1,14 @@
+American Mythology Podcast
+=============================
+
+<a href="www.kodi.tv">KODI</a> / XBMC addon for the audio podcast <a href="www.americanmythologypodcast.com">American Mythology</a><br>
+
+America has its own mythology, and it's about much more than fairy tales, religion, or classic history.<br>
+
+It's our reality and our worldview founded on familiar concepts like freedom, democracy, and opportunity, as well as embedded in our heroes and symbols. There are darker sides to our mythology, too. Each episode blends history and storytelling to explore unique aspects of American identity and ideology that reveal something about our mythology, but also help frame current social and political issues.<br>
+
+Understanding our mythology is key to discovering who we are and defining who we want to be.<br>
+ 
+^^^ To install this addon, either use the <a href="https://www.tvaddons.co/github-browser-kodi/">Kodi Github installer</a> addon or save the .zip file downloaded from the 'clone or download' button to somewhere the Kodi can access (e.g. network drive or USB stick). Then on the Kodi, go to addons > install from zip file.<br>
+
+<a href="https://www.americanmythologypodcast.com"><img src="https://images.squarespace-cdn.com/content/v1/5671a4705a5668ba6b111b2b/1481808815260-7BX8BSX93KDUPWPUEBEJ/ke17ZwdGBToddI8pDm48kOyctPanBqSdf7WQMpY1FsRZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpyD4IQ_uEhoqbBUjTJFcqKvko9JlUzuVmtjr1UPhOA5qkTLSJODyitRxw8OQt1oetw/image-asset.png?format=500w">
---
+++ addon.py
@@ -0,0 +1,61 @@
+import xbmc
+#from kodi_six import xbmc, xbmcaddon, xbmcgui, xbmcplugin, xbmcvfs
+#from xbmc import Plugin, xbmcgui
+from xbmcswift2 import Plugin, xbmcgui
+#from kodi_six import Plugin
+from resources.lib import mainaddon
+
+URL = "http://www.americanmythologypodcast.com/episodes"
+
+plugin = Plugin()
+
+@plugin.route('/')
+def main_menu():
+    """
+    main menu 
+    """
+    items = [
+        {
+            'label': plugin.get_string(30001), 
+            'path': plugin.url_for('all_episodes1'),
+            'thumbnail': "resources/media/icon.png"},
+        {
+            'label': plugin.get_string(30000), 
+            'path': plugin.url_for('all_episodes'),
+            'thumbnail': "resources/media/icon.png"},
+        {
+            'label': plugin.get_string(30002),
+            'path': plugin.url_for('new_to_criminal'),
+            'thumbnail': "resources/media/icon.png"},
+    ]
+
+    return items
+
+@plugin.route('/all_episodes/')
+def all_episodes():
+    """
+    contains playable podcasts listed as just-in
+    """
+    soup = mainaddon.get_soup(URL)
+    
+    playable_podcast = mainaddon.get_playable_podcast(soup)
+    
+    items = mainaddon.compile_playable_podcast(playable_podcast)
+
+    return items
+
+@plugin.route('/all_episodes1/')
+def all_episodes1():
+    """
+    contains playable podcasts listed as just-in
+    """
+    soup = mainaddon.get_soup(URL)
+    
+    playable_podcast1 = mainaddon.get_playable_podcast1(soup)
+    
+    items = mainaddon.compile_playable_podcast1(playable_podcast1)
+
+    return items
+
+if __name__ == '__main__':
+    plugin.run()
