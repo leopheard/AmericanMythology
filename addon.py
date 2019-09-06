fde06f4f1a0c0efb81210b6df49da4571504a43f
leopheard <leopheard.misc@gmail.com>
2019-09-06 16:56:36 -0400

    upload

--- addon.py
+++ addon.py
@@ -5,52 +5,35 @@ from xbmcswift2 import Plugin, xbmcgui
 #from kodi_six import Plugin
 from resources.lib import mainaddon
 
-URL = "http://www.americanmythologypodcast.com/episodes"
+URL = "http://www.americanmythologypodcast.com/episodes?format=RSS"
 
 plugin = Plugin()
-
 @plugin.route('/')
 def main_menu():
-    """
-    main menu 
-    """
     items = [
         {
             'label': plugin.get_string(30001), 
             'path': plugin.url_for('all_episodes1'),
-            'thumbnail': "resources/media/icon.png"},
+            'thumbnail': "https://images.squarespace-cdn.com/content/5671a4705a5668ba6b111b2b/1481812482959-FT137O7ZRUC6ERWMRA0R/AMLogo-1400x1400.png"},
         {
             'label': plugin.get_string(30000), 
             'path': plugin.url_for('all_episodes'),
-            'thumbnail': "resources/media/icon.png"},
+            'thumbnail': "https://images.squarespace-cdn.com/content/5671a4705a5668ba6b111b2b/1481812482959-FT137O7ZRUC6ERWMRA0R/AMLogo-1400x1400.png"},
     ]
-
-    return items
-
-@plugin.route('/all_episodes/')
-def all_episodes():
-    """
-    contains playable podcasts listed as just-in
-    """
-    soup = mainaddon.get_soup(URL)
-    
-    playable_podcast = mainaddon.get_playable_podcast(soup)
-    
-    items = mainaddon.compile_playable_podcast(playable_podcast)
-
     return items
 
 @plugin.route('/all_episodes1/')
 def all_episodes1():
-    """
-    contains playable podcasts listed as just-in
-    """
     soup = mainaddon.get_soup(URL)
-    
     playable_podcast1 = mainaddon.get_playable_podcast1(soup)
-    
     items = mainaddon.compile_playable_podcast1(playable_podcast1)
+    return items
 
+@plugin.route('/all_episodes/')
+def all_episodes():
+    soup = mainaddon.get_soup(URL)
+    playable_podcast = mainaddon.get_playable_podcast(soup)
+    items = mainaddon.compile_playable_podcast(playable_podcast)
     return items
 
 if __name__ == '__main__':
--- resources/lib/mainaddon.py
+++ resources/lib/mainaddon.py
@@ -3,64 +3,38 @@ import re
 from bs4 import BeautifulSoup
 
 def get_soup(url):
-    """
-    @param: url of site to be scraped
-    """
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
-#    xbmc.log('type: %s'%(type(soup)),xbmc.LOGDEBUG)
-    
+    xbmc.log('type: %s'%(type(soup)),xbmc.LOGDEBUG)
     print("type: ", type(soup))
     return soup
-
-get_soup("http://www.americanmythologypodcast.com/episodes")
-
+get_soup("http://www.americanmythologypodcast.com/episodes?format=RSS")
 
 def get_playable_podcast(soup):
-    """
-    @param: parsed html page            
-    """
     subjects = []
-
-    for content in soup.find_all('div'):
-        
+    for content in soup.find_all('item'):
         try:        
-            link = content.find({'class': 'data-url'})
-            link = link.get()
+            link = content.find('enclosure')
+            link = link.get('url')
             print("\n\nLink: ", link)
-
-            title = content.find({'class': 'title-url'})
+            title = content.find('title')
             title = title.get_text()
-
 #            desc = content.find('itunes:subtitle')
 #            desc = desc.get_text()
-            
 #            thumbnail = content.find('itunes:image')
 #            thumbnail = thumbnail.get('href')
-
         except AttributeError:
             continue
-              
         item = {
                 'Link': link,
                 'title': title,
 #                'desc': desc,
-                'thumbnail': "resources/media/icon.png"
+                'thumbnail': "https://images.squarespace-cdn.com/content/5671a4705a5668ba6b111b2b/1481812482959-FT137O7ZRUC6ERWMRA0R/AMLogo-1400x1400.png"
         }
-        
         subjects.append(item) 
-    
     return subjects
-
-
-
-
 def compile_playable_podcast(playable_podcast):
-    """
-    @para: list containing dict of key/values pairs for playable podcasts
-    """
     items = []
-
     for podcast in playable_podcast:
         items.append({
             'label': podcast['title'],
@@ -69,53 +43,33 @@ def compile_playable_podcast(playable_podcast):
 #            'info': podcast['desc'],
             'is_playable': True,
     })
-
     return items
 
-
 def get_playable_podcast1(soup):
-    """
-    @param: parsed html page            
-    """
     subjects = []
-
-    for content in soup.find_all('div', limit=5):
-        
+    for content in soup.find_all('item', limit=9):
         try:        
-            link = content.find({'class': 'data-url'})
-            link = link.get()
+            link = content.find('enclosure')
+            link = link.get('url')
             print("\n\nLink: ", link)
-
-            title = content.find({'class': 'title-url'})
+            title = content.find('title')
             title = title.get_text()
-
 #            desc = content.find('itunes:subtitle')
 #            desc = desc.get_text()
-            
 #            thumbnail = content.find('itunes:image')
 #            thumbnail = thumbnail.get('href')
-
         except AttributeError:
             continue
-              
         item = {
                 'Link': link,
                 'title': title,
 #                'desc': desc,
-                'thumbnail': "resources/media/icon.png"
+                'thumbnail': "https://images.squarespace-cdn.com/content/5671a4705a5668ba6b111b2b/1481812482959-FT137O7ZRUC6ERWMRA0R/AMLogo-1400x1400.png"
         }
-        
         subjects.append(item) 
-    
     return subjects
-
-
 def compile_playable_podcast1(playable_podcast1):
-    """
-    @para: list containing dict of key/values pairs for playable podcasts
-    """
     items = []
-
     for podcast in playable_podcast1:
         items.append({
             'label': podcast['title'],
@@ -124,6 +78,5 @@ def compile_playable_podcast1(playable_podcast1):
 #            'info': podcast['desc'],
             'is_playable': True,
     })
-
     return items
 
